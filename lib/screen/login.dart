530df9546b0dd048a413f8dd1d8e501475b92213
Arun Singh <arunsingh28aug.as@gmail.com>
2024-11-18 16:29:28 +0530

    Refactor login screen and main app widget
    - Refactor the login screen to improve code readability and organization
    - Update the main app widget to use const constructors and add a dark theme

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,86 +6,136 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange[100],
-        automaticallyImplyLeading: false,
-        flexibleSpace: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Builder(
-                    builder: (context) => IconButton(
-                          icon: const Icon(
-                            Icons.menu,
-                            size: 30.2,
-                          ),
-                          onPressed: () {
-                            Scaffold.of(context).openDrawer();
-                            // Open the drawer
-                          },
-                        )),
-                IconButton(
-                  icon: const Icon(
-                    Icons.supervised_user_circle_outlined,
-                    size: 30.2,
+        appBar: AppBar(
+          backgroundColor: Colors.orange[100],
+          automaticallyImplyLeading: false,
+          flexibleSpace: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Builder(
+                      builder: (context) => IconButton(
+                            icon: const Icon(
+                              Icons.menu,
+                              size: 30.2,
+                            ),
+                            onPressed: () {
+                              Scaffold.of(context).openDrawer();
+                              // Open the drawer
+                            },
+                          )),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.supervised_user_circle_outlined,
+                      size: 30.2,
+                    ),
+                    onPressed: () {
+                      // Close the screen
+                    },
                   ),
-                  onPressed: () {
-                    // Close the screen
-                  },
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.orange,
-              ),
-              child: Text(
-                'Hello, Arun Singh',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello, Arun Singh',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text('dev.arunsingh28@gmail.com',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        )),
+                  ],
                 ),
               ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {
+                  // Navigate to Home
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                onTap: () {
+                  // Navigate to Settings
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Logout'),
+                onTap: () {
+                  // Handle Logout
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87),
+              ),
             ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                // Navigate to Home
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                // Navigate to Settings
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: const Text('Logout'),
-              onTap: () {
-                // Handle Logout
-                Navigator.pop(context);
-              },
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle Login
+                    },
+                    child: const Text('Login'),
+                  ),
+                ],
+              ),
             ),
           ],
-        ),
-      ),
-      body: const Center(
-        child: Text('Login Page'),
-      ),
-    );
+        ));
   }
 }
